a6d0d19764ebb3cb7c181ffbf8d60c9d083b6e62
MagmMe <marcin@magm.me>
2020-06-30 07:51:06 +0200

    Initial Commit

---
+++ FamilyKCH/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FamilyKCH
+//
+//  Created by Marcin Magiera on 30/06/2020.
+//  Copyright © 2020 Magme Agency. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

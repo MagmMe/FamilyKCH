46ebab3b5c77fc29956e5373c381974542de4d5c
MagmMe <marcin@magm.me>
2020-07-02 07:49:15 +0200

    Creating Map View file in Swift UI View

---
+++ FamilyKCH/MapView.swift
@@ -0,0 +1,21 @@
+//
+//  MapView.swift
+//  FamilyKCH
+//
+//  Created by Marcin Magiera on 02/07/2020.
+//  Copyright © 2020 Magme Agency. All rights reserved.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
